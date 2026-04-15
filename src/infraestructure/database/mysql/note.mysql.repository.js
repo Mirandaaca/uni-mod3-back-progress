5e787c42219bc42f61c2a0942f316724b0adce79
Cristopher Miranda <cristopher1012z@gmail.com>
2026-04-15 19:46:19 -0400

    Modifique y simplifique el archivo de repositorio de note.repository.mysql y separe el modelo en otro archivo

---
+++ src/infraestructure/database/mysql/note.mysql.repository.js
@@ -0,0 +1,21 @@
+import { NoteModel } from "./note.model.js";
+
+export default class NoteMySQLRepository {
+    async save(noteEntity) {
+        // Se llama al metodo create de sequelize, y se le pasan los parametros necesarios para crear un objeto, y se retorna el resultado
+        const note = await NoteModel.create({
+            title: noteEntity.title,
+            content: noteEntity.content,
+            imageUrl: noteEntity.imageUrl,
+            isPrivate: noteEntity.isPrivate,
+            password: noteEntity.password,
+            userId: noteEntity.userId
+        });
+        // Se llama al metodo toJSON de sequelize, para convertir el objeto note a un objeto plano, y se retorna el resultado
+        return note.toJSON();
+    }
+    async findByUserId(userId) {
+        // Se llama al metodo findAll de sequelize, y se le pasa el userId, para que busque las notas del usuario en la base de datos, y se retorna el resultado
+        return await NoteModel.findAll({ where: { userId } });
+    }
+}
